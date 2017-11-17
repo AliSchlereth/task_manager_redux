544cbab7fde13508fd89c039b0e1e4959e101172
Ali Schlereth <aschlere@sbcglobal.net>
2017-11-17 12:09:08 -0700

    add new and post methods

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -5,9 +5,19 @@ class TaskManagerApp < Sinatra::Base
     erb :dashboard
   end
 
-  get './tasks' do
+  get '/tasks' do
     @tasks = ["task1", "task2", "task3"]
     erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
   end
 
 end
