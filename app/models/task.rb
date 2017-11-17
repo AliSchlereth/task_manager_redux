056f34e0a17aa3b1a58821bd48fcd2a8822cc9d0
Ali Schlereth <aschlere@sbcglobal.net>
2017-11-17 13:12:43 -0700

    Refactor edit links

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,8 +17,8 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new("db/task_manager_development.db")
-    database.results_as_hash = true
+    # database = SQLite3::Database.new("db/task_manager_development.db")
+    # database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
@@ -26,10 +26,27 @@ class Task
   end
 
   def self.find(id)
-    database = SQLite3::Database.new("db/task_manager_development.db")
-    database.results_as_hash = true
+    # database = SQLite3::Database.new("db/task_manager_development.db")
+    # database.results_as_hash = true
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
     Task.new(task)
+  end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE tasks
+                      SET title = ?,
+                          description = ?
+                      WHERE id = ?;",
+                    task_params[:title],
+                    task_params[:description],
+                    id)
+    Task.find(id)
+  end
+
+  def self.database
+    database = SQLite3::Database.new("db/task_manager_development.db")
+    database.results_as_hash = true
+    database
   end
 
 end
